8aec04c1bbcdc1fb34d42186c2c36cc0bea8bd94
Wv98-web <418060258@qq.com>
2023-02-15 10:45:13 +0800

    wishlist 第一次修改

--- assets/wishlist.js
+++ assets/wishlist.js
@@ -3,48 +3,211 @@ class WishList extends HTMLElement {
 		super();
 
 		this.heart = this.querySelector('heart');
-		this.heart.addEventListener('click', function () {
-			console.log('123');
+		this.heart.addEventListener('click', function (event) {
+			console.log(event.target.dataset)
+		});
+	}
+
+	add() {
+		let addBtns = $('.add_to_wish');
+		let wish_list =
+			localStorage.getItem('wishlist') != null && localStorage.getItem('wishlist') != undefined && localStorage.getItem('wishlist')
+				? JSON.parse(localStorage.getItem('wishlist'))
+				: [];
+
+		addBtns.click(function () {
+			const _self = $(this);
+			const handle = $(this).data('handle');
+
+			$.getJSON(window.Shopify.routes.root + `products/${handle}.js`, function (data) {
+				if (wish_list.length) {
+					JSON.parse(JSON.stringify(wish_list)).map((item) => {
+						if (item.handle != handle) {
+							console.log('增');
+							_self.addClass('checked');
+							wish_list.push(data);
+						} else {
+							console.log('删');
+							_self.removeClass('checked');
+							wish_list = JSON.parse(JSON.stringify(wish_list)).filter((prod) => {
+								return prod.handle != handle;
+							});
+						}
+					});
+				} else {
+					_self.addClass('checked');
+					wish_list.push(data);
+				}
+
+				localStorage.setItem('wishlist', JSON.stringify(wish_list));
+			});
+		});
+	}
+
+	wishListRender() {
+		let addBtns = $('.add_to_wish');
+		const localWishlist = localStorage.getItem('wishlist') != null && localStorage.getItem('wishlist') ? JSON.parse(localStorage.getItem('wishlist')) : [];
+
+		addBtns.map((index, item) => {
+			if (localWishlist.length) {
+				localWishlist.map((product) => {
+					if ($(item).data('handle') == product.handle) {
+						$(item).addClass('checked');
+					}
+				});
+			} else {
+				$(item).removeClass('checked');
+			}
 		});
 
-		// this.detailsContainer = this.querySelector('details');
-		// this.summaryToggle = this.querySelector('summary');
+		const html = localWishlist.map((product) => {
+			const price = Shopify.formatMoney(product.price, '${{amount}}');
+			const compare_at_price = Shopify.formatMoney(product.compare_at_price, '${{amount}}');
 
-		// this.detailsContainer.addEventListener('keyup', (event) => event.code.toUpperCase() === 'ESCAPE' && this.close());
-		// this.summaryToggle.addEventListener('click', this.onSummaryClick.bind(this));
-		// this.querySelector('button[type="button"]').addEventListener('click', this.close.bind(this));
+			return `
+				<li class="grid__item">
+					<div class="card-wrapper product-card-wrapper underline-links-hover card-product-1">
+						<div
+							class="card card--standard card--media"
+							style="--ratio-percent: 100%;"
+						>
+							<div class="card__inner color-background-2 gradient ratio" style="--ratio-percent: 100%;">
+								<div class="card__media">
+									<div class="media media--transparent media--hover-effect">
+										<img
+											src=${product.featured_image}
+											sizes="(min-width: 1200px) 267px, (min-width: 990px) calc((100vw - 130px) / 4), (min-width: 750px) calc((100vw - 120px) / 3), calc((100vw - 35px) / 2)"
+											alt=${product.title}
+											class="motion-reduce"
+											loading="lazy"
+											width="1080"
+											height="1350"
+										>
+									</div>
+								</div>
+								<div class="card__content">
+									<div class="card__information">
+										<h3 class="card__heading">
+											<a
+												href=${product.url}
+												class="full-unstyled-link"
+											>
+												${product.title}
+											</a>
+										</h3>
+									</div>
+						
+									<div class="card__badge top right card__wish add_to_wish" data-handle=${product.handle} style="z-index: 2;">
+										<div class="card__wish-item">
+											<svg
+												viewBox="0 0 20 19"
+												version="1.1"
+												xmlns="http://www.w3.org/2000/svg"
+												xmlns:xlink="http://www.w3.org/1999/xlink"
+											>
+												<title>爱心备份 2</title>
+												<g id="页面-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<g id="详情页" transform="translate(-521.000000, -707.000000)" fill-rule="nonzero" stroke="#EF422B">
+														<g id="爱心备份-2" transform="translate(522.000000, 708.000000)">
+															<path d="M9.00562852,16.7954972 C8.7129456,16.7954972 8.44277674,16.7054409 8.20637898,16.5140713 C8.01500936,16.3564728 3.34333959,12.7429643 1.47467168,9.80487804 C0.85553472,8.83677298 0,7.51969982 0,5.6510319 C0,2.53283303 2.33020638,0 5.1782364,0 C6.63039399,0 8.01500939,0.664165107 9.00562852,1.82363978 C9.98499061,0.652908071 11.358349,0 12.8217636,0 C15.6810507,0 18,2.53283301 18,5.6510319 C18,7.48592871 17.2007505,8.74671669 16.5478424,9.77110695 L16.5253283,9.80487804 C14.6566604,12.7429643 9.98499061,16.3677298 9.79362102,16.5140713 C9.5684803,16.6941839 9.29831146,16.7954972 9.00562852,16.7954972 Z" id="形状"></path>
+														</g>
+													</g>
+												</g>
+											</svg>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="card__content">
+								<div class="card__information">
+									<h3
+										class="card__heading h5"
+									>
+										<a
+											href=${product.url}
+											class="full-unstyled-link"
+											>
+											${product.title}
+										</a>
+									</h3>
+									<div class="card-information">
+										<div class="price  price--on-sale ">
+											<div class="price__container">
+												<div class="price__regular">
+													<span class="price-item price-item--regular"> ${price} </span>
+												</div>
+												<div class="price__sale">
+													<span class="price-item price-item--sale price-item--last"> ${price} </span>
+													${
+														product.price_varies
+															? `<span>
+															<s class="price-item price-item--regular" style="color: #7C7C46;"> ${compare_at_price} </s>
+														</span>`
+															: ``
+													}
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</li>`;
+		});
 
-		// this.summaryToggle.setAttribute('role', 'button');
-	}
-
-	isOpen() {
-		return this.detailsContainer.hasAttribute('open');
-	}
-
-	onSummaryClick(event) {
-		event.preventDefault();
-		event.target.closest('details').hasAttribute('open') ? this.close() : this.open(event);
-	}
-
-	onBodyClick(event) {
-		if (!this.contains(event.target) || event.target.classList.contains('modal-overlay')) this.close(false);
-	}
-
-	open(event) {
-		this.onBodyClickEvent = this.onBodyClickEvent || this.onBodyClick.bind(this);
-		event.target.closest('details').setAttribute('open', true);
-		document.body.addEventListener('click', this.onBodyClickEvent);
-		document.body.classList.add('overflow-hidden');
-
-		trapFocus(this.detailsContainer.querySelector('[tabindex="-1"]'), this.detailsContainer.querySelector('input:not([type="hidden"])'));
-	}
-
-	close(focusToggle = true) {
-		removeTrapFocus(focusToggle ? this.summaryToggle : null);
-		this.detailsContainer.removeAttribute('open');
-		document.body.removeEventListener('click', this.onBodyClickEvent);
-		document.body.classList.remove('overflow-hidden');
+		$('#wishlist').html(html);
 	}
 }
 
 customElements.define('wish-list', WishList);
+
+var Shopify = Shopify || {};
+Shopify.money_format = '{{ shop.money_format }}';
+Shopify.formatMoney = function (cents, format) {
+	if (typeof cents == 'string') {
+		cents = cents.replace('.', '');
+	}
+	var value = '';
+	var placeholderRegex = /{{\s*(\w+)\s*}}/;
+	var formatString = format || this.money_format;
+
+	function defaultOption(opt, def) {
+		return typeof opt == 'undefined' ? def : opt;
+	}
+
+	function formatWithDelimiters(number, precision, thousands, decimal) {
+		precision = defaultOption(precision, 2);
+		thousands = defaultOption(thousands, ',');
+		decimal = defaultOption(decimal, '.');
+
+		if (isNaN(number) || number == null) {
+			return 0;
+		}
+
+		number = (number / 100.0).toFixed(precision);
+
+		var parts = number.split('.'),
+			dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+			cents = parts[1] ? decimal + parts[1] : '';
+
+		return dollars + cents;
+	}
+
+	switch (formatString.match(placeholderRegex)[1]) {
+		case 'amount':
+			value = formatWithDelimiters(cents, 2);
+			break;
+		case 'amount_no_decimals':
+			value = formatWithDelimiters(cents, 0);
+			break;
+		case 'amount_with_comma_separator':
+			value = formatWithDelimiters(cents, 2, '.', ',');
+			break;
+		case 'amount_no_decimals_with_comma_separator':
+			value = formatWithDelimiters(cents, 0, '.', ',');
+			break;
+	}
+
+	return formatString.replace(placeholderRegex, value);
+};
